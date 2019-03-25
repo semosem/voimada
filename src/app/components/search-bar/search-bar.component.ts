897074e7103160387ea2efe167c86a0944ef1b00
sem.gebresilassie@gmail.com <sem.gebresilassie@gmail.com>
2019-03-25 18:47:08 +0200

    add search mechanism

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from "@angular/core";
+import { JobService } from "../../services/jobs.service";
+import { Jobs } from "../../types/types";
 
 @Component({
-  selector: 'app-search-bar',
-  templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.css']
+  selector: "app-search-bar",
+  templateUrl: "./search-bar.component.html",
+  styleUrls: ["./search-bar.component.css"]
 })
-export class SearchBarComponent implements OnInit {
+export class SearchBarComponent {
+  private query: string;
+  public dataSource: Jobs[];
+  public showList: boolean = false;
 
-  constructor() { }
+  constructor(private _jobService: JobService) {}
 
-  ngOnInit() {
+  searchJobs(query) {
+    this._jobService
+      .getJobs(query)
+      .subscribe(
+        jobsInfo => this.handlResolve(jobsInfo),
+        err => this.handleReject(err),
+        () => console.log("resting")
+      );
   }
 
+  handlResolve(jobsInfo): void {
+    const jobs = jobsInfo.SearchResult.SearchResultItems.map(job => ({
+      MatchedObjectId: job.MatchedObjectId,
+      OrganizationName: job.MatchedObjectDescriptor.OrganizationName,
+      PositionTitle: job.MatchedObjectDescriptor.PositionTitle,
+      PositionURI: job.MatchedObjectDescriptor.PositionURI,
+      PositionStartDate: job.MatchedObjectDescriptor.PositionStartDate,
+      PositionEndDate: job.MatchedObjectDescriptor.PositionEndDate,
+      PositionLocationDisplay:
+        job.MatchedObjectDescriptor.PositionLocationDisplay
+    }));
+    this.dataSource = jobs;
+    this.showList = true;
+  }
+
+  handleReject(err): void {
+    console.log(err);
+  }
 }
