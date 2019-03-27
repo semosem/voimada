ec91ac498bdc826fb49c52a0fe70b9ca8fdba354
sem.gebresilassie@gmail.com <sem.gebresilassie@gmail.com>
2019-03-27 23:12:22 +0200

    make it be

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.use(express.static(__dirname + "/dist"));
+app.use(express.static(__dirname + "/dist/home-task-voimada"));
 
 app.listen(process.env.PORT || 8080);
 
