15f27ac6e3f08c703319d92de627bea3284d6585
sem.gebresilassie@gmail.com <sem.gebresilassie@gmail.com>
2019-04-23 14:08:14 +0300

    coordinate problem simple solution

--- server.js
+++ server.js
@@ -1,12 +1,50 @@
-const express = require("express");
-const app = express();
-const path = require("path");
+const theArr = [
+  { x: -1, y: -2 },
+  { x: 1, y: 2 },
+  { x: 2, y: 4 },
+  { x: -3, y: 2 },
+  { x: 2, y: -2 }
+];
 
-app.use(express.static(__dirname + "/dist/home-task-voimada"));
+const slopes = theArr.map((coor, index) => ({
+  index: index
+}));
 
-app.listen(process.env.PORT || 8080);
+function separageQuads(anArray) {
+  let count = 0;
+  let slopes = [];
+  anArray.map(coor => {
+    if (coor.x > 0 && coor.y > 0) {
+      let y = 0 - coor.y;
+      let x = 0 - coor.x;
+      if (slopes.indexOf(y / x) === -1) {
+        slopes.push(y / x);
+        count++;
+      }
+    } else if (coor.x < 0 && coor.y > 0) {
+      let y = 0 - coor.y;
+      let x = 0 - coor.x;
+      if (slopes.indexOf(y / x) === -1) {
+        slopes.push(y / x);
+        count++;
+      }
+    } else if (coor.x < 0 && coor.y < 0) {
+      let y = 0 - coor.y;
+      let x = 0 - coor.x;
+      if (slopes.indexOf(y / x) === -1) {
+        slopes.push(-y / x);
+        count++;
+      }
+    } else {
+      let y = 0 - coor.y;
+      let x = 0 - coor.x;
+      if (slopes.indexOf(y / x) === -1) {
+        slopes.push(-y / x);
+        count++;
+      }
+    }
+  });
+  console.log(count);
+}
 
-app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname + "/dist/home-task-voimada/index.html"));
-});
-console.log("listening");
+separageQuads(theArr);
